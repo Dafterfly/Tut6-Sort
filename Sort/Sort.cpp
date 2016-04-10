34da0c60670c9e2e053fbb6e80d091258f95958b
Eustacia Rajman <eustacia_rajman@telkomsa.net>
2016-04-10 14:51:15 +0200

    Add sorting testing and printouts of the results

--- Sort/Sort.cpp
+++ Sort/Sort.cpp
@@ -15,7 +15,7 @@ void sort(T arr[], int len)   // sorts an array gven array and length
 	{
 		for (int j = i + 1; j < len; j++)
 		{
-			if (arr1[i] < arr1[j])
+			if (arr[i] < arr[j])
 			{
 				// Swapping
 				temp = arr[i];
@@ -31,8 +31,34 @@ int main()
 	// Make different arrays:
 	string strArr[5] = { "Thomas", "Guy-Man", "Anderson", "Michael", "Joachim" };
 	int intArr[5] = { 6, 9, 2, 4, 3 };
-	float floarArr[5] = { 5.3, 2.2, 7.1, 1.4, 8.0 };
+	float floatArr[5] = { 5.3, 2.2, 7.1, 1.4, 8.0 };
 
+	// sort arrays:
+	sort(strArr, 5);
+	sort(intArr, 5);
+	sort(floatArr, 5);
 
+	int i;
 
+	// Testing printouts
+	cout << "Testing string sort:" << endl;
+	for (i = 0; i < 5; i++) 
+	{
+		cout << strArr[i] << endl;
+	}
+	cout << endl << endl;
+
+	cout << "Testing int sort:" << endl;
+	for (i = 0; i < 5; i++)
+	{
+		cout << intArr[i] << endl;
+	}
+	cout << endl << endl;
+
+	cout << "Testing float sort:" << endl;
+	for (i = 0; i < 5; i++)
+	{
+		cout << floatArr[i] << endl;
+	}
+	cout << endl << endl;
 }
